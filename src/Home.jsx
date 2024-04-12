892b4f99dd8561a9a858d7e6061a063771f191eb
Angel <angelherrarte3@gmail.com>
2024-04-11 20:49:12 -0600

    Progress

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Home</h1>
+            <a href='/login'>Login</a>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './Login.css';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Login attempted with username: ${username} and password: ${password}`);
+    };
+
+    return (
+        <div className="login-wrapper">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+                <div>
+                    <a href='/home'>Home</a>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
